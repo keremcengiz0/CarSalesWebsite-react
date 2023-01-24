d53d553bda30da04cb24e46df95ea36f64aba20f
keremcengiz0 <keremcengiz0@hotmail.com>
2023-01-24 22:11:23 +0300

    fixed HomeIcon error.

--- src/components/Sidebar/SidebarUser.js
+++ src/components/Sidebar/SidebarUser.js
@@ -24,7 +24,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function HomeIcon(props: SvgIconProps) {
+function HomeIcon(props) {
   return (
     <SvgIcon {...props}>
       <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
